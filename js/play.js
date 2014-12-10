2f147358490b901f7c65eff810d6a43972b06033
Alex Howington <alex.howington@gmail.com>
2014-12-09 20:15:45 -0600

    Add player animations

--- js/play.js
+++ js/play.js
@@ -5,6 +5,8 @@ var playState = {
 
     this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
     this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('right', [1, 2], 8, true);
+    this.player.animations.add('left', [3, 4], 8, true);
     game.physics.arcade.enable(this.player);
     this.player.body.gravity.y = 500;
 
@@ -45,12 +47,15 @@ var playState = {
   movePlayer: function() {
     if (this.cursor.left.isDown) {
       this.player.body.velocity.x = -200;
+      this.player.animations.play('left');
     }
     else if (this.cursor.right.isDown) {
       this.player.body.velocity.x = 200;
+      this.player.animations.play('right');
     }
     else {
       this.player.body.velocity.x = 0;
+      this.player.frame = 0;
     }
 
     if (this.cursor.up.isDown && this.player.body.touching.down) {
