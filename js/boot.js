2205affd5a2bf0ca6a29f997760938dae8229421
Alex Howington <alex.howington@gmail.com>
2014-12-10 03:13:18 -0600

    Add scaling for mobile

--- js/boot.js
+++ js/boot.js
@@ -8,6 +8,22 @@ var bootState = {
     game.stage.backgroundColor = '#3498db';
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    if (!game.device.desktop) {
+      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+      document.body.style.backgroundColor = '#3498db';
+
+      game.scale.minWidth = 250;
+      game.scale.minHeight = 170;
+      game.scale.maxWidth = 1000;
+      game.scale.maxHeight = 680;
+
+      game.scale.pageAlignHorizontally = true;
+      game.scale.pageAlignVertically = true;
+
+      game.scale.setScreenSize(true);
+    }
+
     game.state.start('load');
   }
 };
